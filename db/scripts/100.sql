f9295223f36b6db8ef686405b2be8195def1fd04
Pro <pro@MacBook-Pro.local>
2019-08-12 09:39:42 +0200

    route table

--- db/scripts/100.sql
+++ db/scripts/100.sql
@@ -6,8 +6,10 @@ primary key (id)
 
 create table Route(
 id int not null AUTO_INCREMENT,
+rock_id int not null,
 name varchar(100),
 description varchar(500),
 difficulty_level varchar(10),
-primary key (id)
+primary key (id),
+FOREIGN KEY (rock_id) REFERENCES Rock(id)
 );
