3ec9dbabf45d2e16c320ff12638e37e238d9b23c
Pro <pro@MacBook-Pro.local>
2019-08-12 09:34:15 +0200

    route table

--- db/scripts/100.sql
+++ db/scripts/100.sql
@@ -2,4 +2,12 @@ create table Rock(
 id int not null AUTO_INCREMENT,
 name varchar(100),
 primary key (id)
-)
+);
+
+create table Route(
+id int not null AUTO_INCREMENT,
+name varchar(100),
+description varchar(500),
+difficulty_level varchar(10),
+primary key (id)
+);
