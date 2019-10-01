67d5af733120bb1a509c0fe41d45f21c8670ec15
Pro <>
2019-10-01 17:41:51 +0200

    baza danych

--- db/scripts/100.sql
+++ db/scripts/100.sql
@@ -8,7 +8,6 @@ create table Sector(
 id int not null AUTO_INCREMENT,
 rock_id int not null,
 name varchar(100),
-image blob,
 primary key (id),
 FOREIGN KEY (rock_id) REFERENCES Rock(id)
 );
@@ -20,6 +19,7 @@ sector_id int not null,
 name varchar(100),
 description varchar(500),
 difficulty_level varchar(10),
+image_name varchar(200),
 
 primary key (id),
 FOREIGN KEY (sector_id) REFERENCES Sector(id)
@@ -49,31 +49,38 @@ insert into Sector ( rock_id, name)
 values ( (select id from Rock where name = 'Dupa Słonia'), 'Płyta Sasa i Ściana Luster');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Enpec', '8r +ST', 'III+');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Enpec', '8r +ST', 'III+', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Enderman', '10r +ST', 'VI.1+');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Enderman', '10r +ST', 'VI.1+', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Minecraft', '9r +ST', 'V');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Minecraft', '9r +ST', 'V', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Ryski Dziędzielewicza', '6r +ST', 'VI.2');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Ryski Dziędzielewicza', '6r +ST', 'VI.2', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Lipczyńska', '8r +ST', 'VI.2+');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Lipczyńska', '8r +ST', 'VI.2+', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Ani me, ani be, ani kukuryku', '9r +ST', 'VI.3');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Ani me, ani be, ani kukuryku', '9r +ST', 'VI.3', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Środek Lipczyńskiej', '9r +ST', 'VI.4/4+');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Środek Lipczyńskiej', '9r +ST', 'VI.4/4+', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Apoteoza wytrysku', '7r +ST', 'VI.4');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Apoteoza wytrysku', '7r +ST', 'VI.4', 'dupa_slonia_lipczynska');
 
 insert into Route ( sector_id, name, description, difficulty_level)
-values ( (select id from Sector where name = 'Lipczyńska 1'), 'Kant Lipczyńskiej', '5r', 'VI');
+values ( (select id from Sector where name = 'Lipczyńska 1'), 'Kant Lipczyńskiej', '5r', 'VI', 'dupa_slonia_lipczynska');
+
+
+insert into Route ( sector_id, name, description, difficulty_level)
+values ( (select id from Sector where name = 'Lipczyńska 2'), 'Kant Lipczyńskiej', '7r', 'VI', 'dupa_slonia_lipczynska2');
+
+insert into Route ( sector_id, name, description, difficulty_level)
+values ( (select id from Sector where name = 'Lipczyńska 2'), 'Niemy cyk', '6r', 'VI.1', 'dupa_slonia_lipczynska2');
 
 
 insert into Rock ( name )
@@ -119,15 +126,219 @@ values (1, 38, 12, 8);
 insert into Route_point (route_id, x_percent, y_percent, order_point)
 values (1, 41, 7, 9);
 
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 26, 70, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 27, 65, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 27, 61, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 30, 49, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 33, 43, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 36, 36, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 37, 30, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 37, 24, 8);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 37, 18, 9);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 39, 14, 10);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (2, 42, 8, 11);
 
-SELECT  *
-FROM 
-(
-(
-Rock as c  JOIN Sector as o on c.id = o.rock_id 
-)join Route as r on o.id = r.sector_id
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 37, 75, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 44, 66, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 48, 59, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 49, 49, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 48, 43, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 49, 35, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 50, 29, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 52, 23, 8);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 54, 14, 9);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (3, 57, 7, 10);
 
-#wyświetla tez pkt 
-)join Route_point as rp on  r.id = route_id
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 57, 72, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 55, 65, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 57, 56, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 56, 49, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 59, 34, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 59, 26, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (4, 59, 15, 7);
 
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 61, 75, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 63, 62, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 65, 56, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 67, 47, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 67, 41, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 70, 34, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 70, 24, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 70, 16, 8);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (5, 69, 9, 9);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 68, 73, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 65, 64, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 63, 62, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 59, 57, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 60, 51, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 61, 44, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 62, 36, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 63, 27, 8);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 64, 18, 9);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (6, 64, 10, 10);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 74, 74, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 73, 66, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 73, 59, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 72, 52, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 69, 47, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 69, 41, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 69, 33, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 70, 24, 8);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 71, 16, 9);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (7, 72, 10, 10);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 81, 77, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 80, 70, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 80, 62, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 79, 52, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 78, 43, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 76, 34, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 76, 29, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (8, 76, 22, 8);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (9, 89, 74, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (9, 89, 64, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (9, 86, 55, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (9, 84, 48, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (9, 83, 41, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (9, 81, 34, 6);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (10, 31, 71, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (10, 41, 1, 2);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (11, 53, 64, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (11, 43, 1, 2);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (12, 44, 76, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (12, 48, 10, 2);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (13, 69, 70, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (13, 48, 10, 2);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (14, 78, 67, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (14, 48, 10, 2);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 49, 74, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 48, 57, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 47, 51, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 46, 44, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 46, 34, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 42, 23, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 38, 15, 7);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (15, 38, 9, 8);
+
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 59, 57, 1);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 57, 48, 2);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 54, 35, 3);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 52, 27, 4);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 49, 19, 5);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 47, 14, 6);
+insert into Route_point (route_id, x_percent, y_percent, order_point)
+values (16, 45, 10, 7);
+
+
+
+
+
+
+
+CREATE INDEX idx_sector_id ON Route (sector_id);
+CREATE INDEX idx_route_id ON Route_point (route_id);
 
--- rock/src/main/java/pl/com/rock/rock/logic/RockRepository.java
+++ rock/src/main/java/pl/com/rock/rock/logic/RockRepository.java
@@ -24,10 +24,7 @@ class RockRepository {
      RockRepository(JdbcTemplate jdbcTemplate) {
          this.jdbcTemplate = jdbcTemplate;
      }
-
-
-
-
+     
     List<Map<String, Object>> findRockWithSectorsWithRoutes(Long rockId, Long sectorId, Long routeId) {
         String query = "SELECT    c.id as rock_id, c.name as rock_name, o.id as sector_id ,  o.name as sector_name,  r.id as route_id, r.name as route_name, description, difficulty_level " +
                 "FROM Rock as c  JOIN Sector as o on c.id = o.rock_id  " +
